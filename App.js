e24ae68c3bdaade046721aa3438a16dd72de3b6c
arunyeldi <arunyeldi@gmail.com>
2025-07-03 18:47:17 +0530

    ready for the creating react app e2

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
-/*
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-<div id="parent">
-    <div id="child">
-        <h1>I'm an h1 tag</h1>
-    </div>
-</div>
-
-*/
-
-
-const parent = React.createElement(
-    "div",
-    { id: "parent"},
+const parent = React.createElement("div", { id: "parent"},
     [React.createElement("div", { id: "child1"},[
-        React.createElement("h1", {}, "I'm an h1 tag"), 
-        React.createElement("h2", {}, "I'm an h2 tag......")
+        React.createElement("h1", {}, "Hello React 👋"), 
+        React.createElement("h2", {}, "I'm an h2 tag>>>>>>>>>>>>")
     ]), React.createElement("div", { id: "child2"},[
         React.createElement("h1", {}, "I'm an h1 tag"), 
         React.createElement("h2", {}, "I'm an h2 tag")
-    ])]
-);
+    ])
+]);
 
 console.log(parent);
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Hello React
+# Hello React 👋
+
+....
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Cashing - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
