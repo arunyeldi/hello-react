b43015c1e14084d623c9ee48b4d957cd5a876b62
arunyeldi <arunyeldi@gmail.com>
2025-07-04 23:43:49 +0530

    Added styling to a paragraph ele

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent"},
     React.createElement("div", { id: "child1"},[
         React.createElement("h1", {}, "Hello React 👋"), 
-        React.createElement("h2", {}, "Very Interesting while learning react")
+        React.createElement("h2", {}, "Very Interesting while learning react"),
+        React.createElement('h1', {}, "Parcel will make app faster with all it's dependencies, and also it will do many more things.")
     ])
 );
 
