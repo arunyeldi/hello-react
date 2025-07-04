0834c4fb34a023f22053b03a1563b52b6e95d32f
arunyeldi <arunyeldi@gmail.com>
2025-07-04 23:48:33 +0530

    ch3

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ const parent = React.createElement("div", { id: "parent"},
     React.createElement("div", { id: "child1"},[
         React.createElement("h1", {}, "Hello React 👋"), 
         React.createElement("h2", {}, "Very Interesting while learning react"),
-        React.createElement('h1', {}, "Parcel will make app faster with all it's dependencies, and also it will do many more things.")
+        React.createElement('h1', {}, ["Parcel will make app faster with all it's dependencies, and also it will do many more things,", 
+        " it splits the code,", " it bundles the code,", " it compress the code"]
+        )
     ])
 );
 
